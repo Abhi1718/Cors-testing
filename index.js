bd4077c84fa91d874f60262ea81b5343f3534a3a
Abhi <programwithabhi@gmail.com>
2022-01-23 17:11:18 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let name = "rushikesh"
+let my_obj={
+    name:"Abhishek",
+    role:"Programmer",
+    id:123,
+    fun:function (){
+     return this.name
+    },
+    arr:(abhishek =>{
+        return this.name
+    })
+}
+console.log("this  is a normal function ")
+abhi= my_obj.fun()
+console.log(abhi)
+console.log("this  is a arrow function ")
+abhi2 = my_obj.arr()
+console.log(abhi2)
